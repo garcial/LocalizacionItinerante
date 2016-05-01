25c5c5f2826b14a85e3e08d27a8003747d06ef87
Luis A. Garcia <garcial@uji.es>
2016-05-01 21:24:27 +0200

    Séptima revisión: EL agente Lanzador ya está completo Falta una revisión de los mensajes que se intercambian los agentes sensores. También se ha incluido el código del Agente Simple (que es el mismo código al 99.9% que el Agente Medio)

--- src/es/uji/coop/Lanzador.java
+++ src/es/uji/coop/Lanzador.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSlider;
@@ -22,31 +23,26 @@ import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
 
+
+/**
+ * 
+ * @author luisamable
+ *  Esta es la clase Lanzador que permite ejecutar la plataforma Jade directamente desde código
+ *  Se basa en el código ejemplo propuesto por Cedric Herpson y que se puede encontrar en la 
+ *  siguiente dirección web: 
+ *  http://herpsonc.eu/docs/README_HowToStartJadeFromSourceCode.pdf  Notas (muy breves)
+ *  http://herpsonc.eu/docs/ y seleccionar el fichero: HowToStartJadeFromSourceCode
+ */
 public class Lanzador {
 
 	private static String hostname = "127.0.0.1"; 
-	private static HashMap<String, ContainerController> containerList=new HashMap<String, ContainerController>();// container's name - container's ref
 	private static List<AgentController> agentList;// agents's ref
 	private static Runtime rt;	
+	private static ContainerController mainContainer;
 	
 	public static void main(String[] args) {
 		// Crea el Gui para introducir los parámetros de la ejecución
 		new Lanzador().new GuiInicial();
-		//1), create the platform (Main container (DF+AMS) + containers + monitoring agents : RMA and SNIFFER)
-		//rt = emptyPlatform(containerList);
-
-		//2) create agents and add them to the platform.
-		//agentList = createAgents(containerList);
-
-//		try {
-//			System.out.println("Press a key to start the agents");
-//			System.in.read();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-
-		//3) launch agents
-//		startAgents(agentList);
 	}
 
 	/**********************************************
@@ -60,20 +56,16 @@ public class Lanzador {
 	 * 
 	 * @return a ref to the platform and update the containerList
 	 */
-	private static Runtime emptyPlatform(HashMap<String, ContainerController> containerList){
+	private static Runtime emptyPlatform(boolean RMA){
 
 		Runtime rt = Runtime.instance();
 
 		// 1) create a platform (main container+DF+AMS)
 		Profile pMain = new ProfileImpl(hostname, 8888, null);
 		System.out.println("Launching a main-container..."+pMain);
-		AgentContainer mainContainerRef = rt.createMainContainer(pMain); //DF and AMS are include
-
-		// 2) create the containers
-		containerList.putAll(createContainers(rt));
-
-		// 3) create monitoring agents : rma agent, used to debug and monitor the platform; sniffer agent, to monitor communications; 
-		createMonitoringAgents(mainContainerRef);
+		mainContainer = rt.createMainContainer(pMain); //DF and AMS are include
+		// Si se ha seleccionado lanzar la ejecución de RMA se recibe un boolean indicándolo: RMA
+		if (RMA) createRMA();
 
 		System.out.println("Plaform ok");
 		return rt;
@@ -81,81 +73,20 @@ public class Lanzador {
 	}
 
 	/**
-	 * Create the containers used to hold the agents 
-	 * @param rt The reference to the main container
-	 * @return an Hmap associating the name of a container and its object reference.
-	 * 
-	 * note: there is a smarter way to find a container with its name, but we go fast to the goal here. Cf jade's doc.
+	 * Crea el agente RMA para monitorización/activación en ejecución de agentes en la plataforma
 	 */
-	private static HashMap<String,ContainerController> createContainers(Runtime rt) {
-		String containerName;
-		ProfileImpl pContainer;
-		ContainerController containerRef;
-		HashMap<String, ContainerController> containerList=new HashMap<String, ContainerController>();//bad to do it here.
 
-
-		System.out.println("Launching containers ...");
-
-		//create the container0	
-		containerName="container0";
-		pContainer = new ProfileImpl(null, 8888, null);
-		System.out.println("Launching container "+pContainer);
-		containerRef = rt.createAgentContainer(pContainer); //ContainerController replace AgentContainer in the new versions of Jade.
-		containerList.put(containerName, containerRef);
-
-		//create the container1	
-		containerName="container1";
-		pContainer = new ProfileImpl(null, 8888, null);
-		System.out.println("Launching container "+pContainer);
-		containerRef = rt.createAgentContainer(pContainer); //ContainerController replace AgentContainer in the new versions of Jade.
-		containerList.put(containerName, containerRef);
-
-		//create the container2	
-		containerName="container2";
-		pContainer = new ProfileImpl(null, 8888, null);
-		System.out.println("Launching container "+pContainer);
-		containerRef = rt.createAgentContainer(pContainer); //ContainerController replace AgentContainer in the new versions of Jade.
-		containerList.put(containerName, containerRef);
-
-		System.out.println("Launching containers done");
-		return containerList;
-	}
-
-
-	/**
-	 * create the monitoring agents (rma+sniffer) on the main-container given in parameter and launch them.
-	 *  - RMA agent's is used to debug and monitor the platform;
-	 *  - Sniffer agent is used to monitor communications
-	 * @param mc the main-container's reference
-	 * @return a ref to the sniffeur agent
-	 */
-	private static void createMonitoringAgents(ContainerController mc) {
-
+	private static void createRMA() {
 		System.out.println("Launching the rma agent on the main container ...");
 		AgentController rma;
 
 		try {
-			rma = mc.createNewAgent("rma", "jade.tools.rma.rma", new Object[0]);
+			rma = mainContainer.createNewAgent("rma", "jade.tools.rma.rma", new Object[0]);
 			rma.start();
 		} catch (StaleProxyException e) {
 			e.printStackTrace();
 			System.out.println("Launching of rma agent failed");
 		}
-
-		System.out.println("Launching  Sniffer agent on the main container...");
-		AgentController snif=null;
-
-		try {
-			snif= mc.createNewAgent("sniffeur", "jade.tools.sniffer.Sniffer",new Object[0]);
-			snif.start();
-
-		} catch (StaleProxyException e) {
-			e.printStackTrace();
-			System.out.println("launching of sniffer agent failed");
-
-		}		
-
-
 	}
 
 
@@ -166,78 +97,59 @@ public class Lanzador {
 	 * 
 	 **********************************************/
 
-
-	/**
-	 *  Creates the agents and add them to the agentList.  agents are NOT started.
-	 *@param containerList :Name and container's ref
-	 *@param sniff : a ref to the sniffeur agent
-	 *@return the agentList
-	 */
-	private static List<AgentController> createAgents(HashMap<String, ContainerController> containerList, 
-			                                          int nFijos, int nMedios, int nSencillos) {
-		System.out.println("Launching agents..." + nFijos + " Fijos; "+nMedios+" Medios; "+nSencillos+" Sencillos.");
-		ContainerController c;
+	private static List<AgentController> createAgents(int nFijos, int nMedios, int nSimples) {
+		System.out.println("Launching agents..." + nFijos + " Fijos; "+nMedios+" Medios; "+nSimples+" Sencillos.");
 		String agentName;
 		List<AgentController> agentList=new ArrayList();
+		
+		// Primero los agentes de la infraestructura
+		
+	    try {
+		    AgentController	ag=mainContainer.createNewAgent("Agente Escenario",AgInterfaz.class.getName(),null);
+		    agentList.add(ag);
+		    System.out.println("Agente Interfaz Escenario launched");
+		    ag=mainContainer.createNewAgent("Agente Sensor",AgSensor.class.getName(),null);
+		    agentList.add(ag);
+		    System.out.println("Agente Sensor launched");
+	    } catch (StaleProxyException e) { e.printStackTrace(); }
+	    
+		//Luego los agentes fijos
 
-		//Agent0 on container0
-		c = containerList.get("container0");
-		agentName="Agent0";
-		try {
-
-			List<String> data=new ArrayList<String>();
-			data.add("1");data.add("2");data.add("3");
-			Object[] objtab=new Object[]{data};//used to give informations to the agent
-			AgentController	ag=c.createNewAgent(agentName,AgInterfaz.class.getName(),objtab);
-			agentList.add(ag);
-			System.out.println(agentName+" launched");
-		} catch (StaleProxyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(int i = 1; i<= nFijos; i++) {
+			agentName="F"+i;		
+		    try {
+		    	// El 3 parámetro que aquí está null es un Object[] para pasarle argumentos al agente
+			    AgentController	ag=mainContainer.createNewAgent(agentName,AgenteFijo.class.getName(),null);
+			    agentList.add(ag);
+			    System.out.println(agentName+" launched");
+		    } catch (StaleProxyException e) { e.printStackTrace(); }
 		}
+	
+		//Ahora los agentes medios
 
-		//Agent1 and Agent2 on container1
-		c = containerList.get("container1");
-		agentName="Agent1";
-		try {					
-			Object[] objtab=new Object[]{};//used to give informations to the agent
-			AgentController	ag=c.createNewAgent(agentName,AgSensor.class.getName(),objtab);
-			agentList.add(ag);
-			System.out.println(agentName+" launched");
-		} catch (StaleProxyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(int i = 1; i<= nMedios; i++) {
+			agentName="M"+i;		
+		    try {
+			    AgentController	ag=mainContainer.createNewAgent(agentName,AgMedio.class.getName(),null);
+			    agentList.add(ag);
+			    System.out.println(agentName+" launched");
+		    } catch (StaleProxyException e) { e.printStackTrace(); }
 		}
+		
+		//Por último los agentes simples
 
-		agentName="Agent2";
-		try {
-			Object[] objtab=new Object[]{};//used to give informations to the agent
-			AgentController	ag=c.createNewAgent(agentName,AgenteFijo.class.getName(),objtab);
-			agentList.add(ag);
-			System.out.println(agentName+" launched");
-		} catch (StaleProxyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-
-		//Agent3 on container2
-		c = containerList.get("container2");
-		agentName="Agent3";
-		try {					
-			Object[] objtab=new Object[]{};//used to give informations to the agent
-			AgentController	ag=c.createNewAgent(agentName,AgMedio.class.getName(),objtab);
-			agentList.add(ag);
-			System.out.println(agentName+" launched");
-		} catch (StaleProxyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(int i = 1; i<= nSimples; i++) {
+			agentName="S"+i;		
+		    try {
+			    AgentController	ag = mainContainer.createNewAgent(agentName,AgSimple.class.getName(),null);
+			    agentList.add(ag);
+			    System.out.println(agentName+" launched");
+		    } catch (StaleProxyException e) { e.printStackTrace(); }
 		}
-
-
-		System.out.println("Agents launched...");
-		return agentList;
+		
+	    return agentList;
 	}
-
+	
 	/**
 	 * Start the agents
 	 * @param agentList
@@ -250,29 +162,27 @@ public class Lanzador {
 		for(final AgentController ac: agentList){
 			try {
 				ac.start();
-			} catch (StaleProxyException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
+			} catch (StaleProxyException e) { e.printStackTrace(); }
 		}
 		System.out.println("Agents started...");
 	}
 
 	private class GuiInicial extends JFrame {
 		
+		private static final long serialVersionUID = 1L;
 		//private JPanel contentPane;
 		JSlider sliderFijos;
 		JSlider sliderMedios;
-		JSlider sliderSencillos;
+		JSlider sliderSimples;
 		JButton botonLanzar;
 		
 		public GuiInicial(){
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setTitle("Parámetros de ejecución: Localizacion compartida");
+			setSize(1200, 400);
 			Container container = this.getContentPane();
 	        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-//	        container.setBounds(10, 10, 400, 800);
+	        container.setBounds(10, 10, 400, 800);
 
 	        JLabel labelFijos = new JLabel("Número de sensores fijos");
 	        labelFijos.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -284,6 +194,7 @@ public class Lanzador {
 			sliderFijos.setPaintLabels(true);
 	        sliderFijos.setAlignmentX(Component.CENTER_ALIGNMENT);
 	        container.add(sliderFijos);
+
 	        JLabel labelMedios = new JLabel("Número de sensores medios");
 	        labelMedios.setAlignmentX(Component.CENTER_ALIGNMENT);
 	        container.add(labelMedios);
@@ -294,24 +205,32 @@ public class Lanzador {
 			sliderMedios.setPaintLabels(true);
 	        sliderMedios.setAlignmentX(Component.CENTER_ALIGNMENT);
 	        container.add(sliderMedios);
-	        JLabel labelSencillos = new JLabel("Número de sensores sencillos");
-	        labelSencillos.setAlignmentX(Component.CENTER_ALIGNMENT);
-	        container.add(labelSencillos);
-			sliderSencillos = new JSlider(JSlider.HORIZONTAL, 3, 200, 5);
-			sliderSencillos.setMajorTickSpacing(10);
-			sliderSencillos.setMinorTickSpacing(1);
-			sliderSencillos.setPaintTicks(true);
-			sliderSencillos.setPaintLabels(true);
-	        sliderSencillos.setAlignmentX(Component.CENTER_ALIGNMENT);
-	        container.add(sliderSencillos);
+
+	        JLabel labelSimples = new JLabel("Número de sensores sencillos");
+	        labelSimples.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        container.add(labelSimples);
+			sliderSimples = new JSlider(JSlider.HORIZONTAL, 3, 200, 5);
+			sliderSimples.setMajorTickSpacing(50);
+			sliderSimples.setMinorTickSpacing(1);
+			sliderSimples.setPaintTicks(true);
+			sliderSimples.setPaintLabels(true);
+			sliderSimples.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        container.add(sliderSimples);
+
+	        JCheckBox checkRMA = new JCheckBox("¿Activar RMA?");
+	        checkRMA.setAlignmentX(Component.CENTER_ALIGNMENT);
+	        container.add(checkRMA);
+	        
 			botonLanzar = new JButton("Lanzar ejecución");
 			botonLanzar.addActionListener(new ActionListener() {
 				
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					rt = emptyPlatform(containerList);
-					agentList = createAgents(containerList, sliderFijos.getValue(), 
-							sliderMedios.getValue(), sliderSencillos.getValue());
+					dispose();
+					rt = emptyPlatform(checkRMA.isSelected());
+					agentList = createAgents(sliderFijos.getValue(), 
+							                 sliderMedios.getValue(), 
+							                 sliderSimples.getValue());
      				startAgents(agentList);
 				}
 			});
